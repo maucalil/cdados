6f679eed8b4a2670149ad850fba04677481e9eb4
Mauricio C. M. Lucera <mauricio.calil@usp.br>
2023-10-30 17:55:03 -0300

    Eu tentei...

--- aula12/main.py
+++ aula12/main.py
@@ -1,25 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(42)
-num_points = 100
 
-x_a = np.random.uniform(1, 50, num_points)
-x_b = np.random.uniform(50, 100, num_points)
-y_b = np.random.uniform(1, 50, num_points)
-y_a = np.random.uniform(50, 100, num_points)
+def main():
+    np.random.seed(42)
+    num_points = 100
 
-dataA = np.array([[x, y] for x,y in zip(x_a, y_a)])
-dataB = np.array([[x, y] for x, y in zip(x_b, y_b) ])
+    mean_A = [25, 75]
+    cov_A = [[100, 0], [0, 100]]
 
-w = np.array([1.0, 5.0])
+    mean_B = [75, 25]
+    cov_B = [[100, 0], [0, 100]]
 
-def print_graphics(dataA, dataB):
-  plt.scatter(dataA[:, 0], dataA[:, 1], label='Class A', color='blue')
-  plt.scatter(dataB[:, 0], dataB[:, 1], label='Class B', color='red')
+    dataA = np.random.multivariate_normal(mean_A, cov_A, num_points)
+    dataB = np.random.multivariate_normal(mean_B, cov_B, num_points)
 
-  plt.xlabel('X-axis')
-  plt.ylabel('Y-axis')
-  plt.legend()
+    best_w = None
+    best_error = float("inf")
 
-  plt.show()
+    for w1 in range(0, 1):
+        for w2 in range(-50, 51):
+            w = np.array([w1, w2])
+            errorA = calc_error(dataA, w)
+            errorB = calc_error(dataB, w, positive=False)
+            error = (errorA + errorB) / 2
+
+            if error < best_error:
+                best_error = error
+                best_w = w
+
+    print(f"Melhores coeficientes: w1 = {best_w[0]}, w2 = {best_w[1]}")
+    print(f"Erro mínimo: {best_error}")
+    print_graphics(dataA, dataB, best_w)
+
+def print_graphics(dataA, dataB, w=None):
+    plt.scatter(dataA[:, 0], dataA[:, 1], label="Class A", color="blue")
+    plt.scatter(dataB[:, 0], dataB[:, 1], label="Class B", color="red")
+
+
+    if w is not None:
+        x_boundary = np.linspace(0, 100, 100)
+        y_boundary = (-w[0] / w[1]) * x_boundary
+        plt.plot(x_boundary, y_boundary, label='Boundary', color='green')
+
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.legend()
+
+    plt.show()
+
+def calc_error(data, w, positive = True):
+    if positive:
+        classes = np.dot(data, w) > 0 # pontos acima da reta
+    else:
+        classes = np.dot(data, w) <= 0 # pontos abaixo da reta
+    
+    errors = np.sum(classes is False)
+
+    return errors/len(classes)
+
+
+if __name__ == "__main__":
+    main()
