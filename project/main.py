0de572730701fe7932bd3cbee67a23cf3c37f536
Maurício Calil Manfrim Lucera <mauricio.calil@usp.br>
2023-11-30 17:29:07 -0300

    Finalize project

--- project/main.py
+++ project/main.py
@@ -8,10 +8,10 @@ from sklearn.svm import SVC
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
 
 def main():
-    # Load data from file
+    # Carrega os dados do arquivo
     data_df = load_data("data.csv")
 
-    # Normalize data
+    # Normaliza os dados
     normalized_data_df = normalize_data(data_df)
 
     # Extract features and target variable
@@ -19,15 +19,14 @@ def main():
     y = normalized_data_df["class"] # targets
     class_labels = y.unique()
 
-    correlation_matrix = X.corr()
-    # Calcular as médias dos coeficientes para cada classe
+    # Calcula a media dos coeficientes para cada classe
     class_means = normalized_data_df.groupby('class').mean()
 
-    # Calcular a matriz de correlação entre as classes
+    # Calcula a matriz de correlação entre as classes
     class_corr_matrix = class_means.T.corr()
     plot_correlation_matrix(class_corr_matrix)
 
-    # Create the models
+    # Cria os modelos
     mlp = MLPClassifier(**get_mlp_parameters())
     qda = QuadraticDiscriminantAnalysis()
     lda = LinearDiscriminantAnalysis()
@@ -37,10 +36,10 @@ def main():
     results = []
 
     for model in models:
-        # Train and test the model using cross validation
+        # Treino e teste do modelo utilizando validacao cruzada
         y_pred = cross_val_predict(model, X, y, cv=5)
 
-        # Model metrics (precision, recall, f1-score, acuracia)
+        # Metricas do modelo (acuracia, precisao, recall, f1-score)
         report = classification_report(y, y_pred, target_names=class_labels, output_dict=True)
         metrics = get_metrics_from_report(report)
         results.append({
@@ -48,34 +47,33 @@ def main():
             **metrics
         })
 
-        # Confusion matrix
+        # Matriz de confusão
         cm = confusion_matrix(y, y_pred, labels=class_labels)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_labels)
         disp.plot()
         plt.show()
     
-    # Save results to a csv
+    # Salva os resultados para um CSV
     results_df = pd.DataFrame(results)
     results_df.to_csv('results.csv', index=False)
 
-
-# Loads data from a CSV file into a Pandas DataFrame.
+# Carrega dados do CSV em um Pandas DataFrame
 def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
-# Normalizes features using Min-Max scaling
+# Normaliza as features usando escalonamento Min-Max
 def normalize_data(data: pd.DataFrame, range=(-1, 1)):
     features = data.drop(["audio_name", "class"], axis=1)
     scaler = MinMaxScaler(range)
     normalized_features = scaler.fit_transform(features)
 
-    # Create a new DataFrame with normalized features and the target variable
+    # Cria um novo DataFrame com as features normalizadas
     data_normalized = pd.DataFrame(data=normalized_features, columns=features.columns)
     data_normalized["class"] = data["class"]
     return data_normalized
 
-# Define the mlp parameters
+# Define os parametros do MLP
 def get_mlp_parameters():
     return {
         'hidden_layer_sizes': (14),
@@ -88,18 +86,18 @@ def get_mlp_parameters():
     }
 
 def plot_correlation_matrix(correlation_matrix):
-    # Define figure size
+    # Define o tamanho da figura
     fig, ax = plt.subplots(figsize=(10, 8))
 
-    # Create heatmap
+    # Cria um mapa de calor
     cax = ax.matshow(correlation_matrix, cmap='coolwarm')
     fig.colorbar(cax)
 
-    # Configure axis labels
+    # Configura os labels
     plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=90)
     plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 
-    # Add numeric value
+    # Adiciona o valor numerico no quadrado
     for i in range(len(correlation_matrix.columns)):
         for j in range(len(correlation_matrix.columns)):
             ax.text(j, i, f'{correlation_matrix.iloc[i, j]:.2f}', ha='center', va='center', color='w')
@@ -107,6 +105,7 @@ def plot_correlation_matrix(correlation_matrix):
     plt.title("Matriz de Correlação")
     plt.show()
 
+# Extrai as metricas do dicionario
 def get_metrics_from_report(report):
     avg_precision = report['weighted avg']['precision']
     avg_recall = report['weighted avg']['recall']
